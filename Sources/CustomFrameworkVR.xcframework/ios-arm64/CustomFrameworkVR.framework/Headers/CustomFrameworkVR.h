c7210371c6400a11c8a48c1e7c8792aa6f14562a
Vivek <vivek@visio-apps.com>
2024-09-03 17:46:53 +0530

    Version 1.1.0

---
+++ Sources/CustomFrameworkVR.xcframework/ios-arm64/CustomFrameworkVR.framework/Headers/CustomFrameworkVR.h
@@ -0,0 +1,18 @@
+//
+//  CustomFrameworkVR.h
+//  CustomFrameworkVR
+//
+//  Created by Visioapps on 03/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFrameworkVR.
+FOUNDATION_EXPORT double CustomFrameworkVRVersionNumber;
+
+//! Project version string for CustomFrameworkVR.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkVRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFrameworkVR/PublicHeader.h>
+
+
